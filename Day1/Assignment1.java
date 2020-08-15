bf1450c311edadb19f3e2303994b0dfca65504a5
Arnav Deshmukh <55059620+arnav157@users.noreply.github.com>
2020-08-15 23:55:53 +0530

    Add files via upload
    assignment 1

---
+++ Day1/Assignment1.java
@@ -0,0 +1,13 @@
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		
+		float prod1 = 12;
+		float prod2 = 13;
+		float prod3 = 15;
+		float tax = 10;
+		float finalSum = prod1 + prod2 + prod3;
+		float finalAmount = (tax/100)*finalSum;
+		System.out.println("Final Amount"+" "+ finalAmount);
+	}
+}
